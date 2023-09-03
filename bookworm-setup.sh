7a6671248fe1e04b07ae5e0ce172cce4042226f9
Ted Lavarias <constructingcode@gmail.com>
2023-09-03 17:54:00 -0500

    Added kitty terminal to my setup

--- bookworm-setup.sh
+++ bookworm-setup.sh
@@ -204,11 +204,29 @@ if [ ! -f "$script_dir/status/bash_set_up" ]; then
 	fi
 	confirm_cmd "cp -av $bash_aliases_path $HOME/.bash_aliases"
 	echo
+	echo 'Setting up kitty terminal to use color prompt...'
+	confirm_cmd "sed -i \"s/xterm-color|\\\*-256color) color_prompt=yes;;/xterm-color|*-256color|xterm-kitty) color_prompt=yes;;/\" $HOME/.bashrc"
+	echo
 	echo 'Setting up bash prompt to display git branch, if exists...'
 	confirm_cmd "sed -i \"s~\(if \[ \\\"\\\$color_prompt\\\" = yes \]; then\)~function parse_git_branch {\\\\n\ \ \ \ git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\\\\\\\(.*\\\\\\\\)/ (\\\\\\\\1)/'\\\\n}\\\\n\1~\" $HOME/.bashrc"
 	confirm_cmd "sed -i \"s/PS1='\\\${debian_chroot:+(\\\$debian_chroot)}.*033.*/PS1=\\\"\\\\\\\\[\\\\\\\\033[34m\\\\\\\\]\\\\\\\\D{%Y%m%d}\\\\\\\\[\\\\\\\\033[00m\\\\\\\\]T\\\\\\\\[\\\\\\\\033[34m\\\\\\\\]\\\\\\\\D{%H%M} \\\${debian_chroot:+(\\\$debian_chroot)}\\\\\\\\[\\\\\\\\033[01;32m\\\\\\\\]\\\\\\\\u@\\\\\\\\h\\\\\\\\[\\\\\\\\033[00m\\\\\\\\]:\\\\\\\\[\\\\\\\\033[01;34m\\\\\\\\]\\\\\\\\w\\\\\\\\[\\\\\\\\033[0;33m\\\\\\\\]\\\\\\\\\\\$(parse_git_branch)\\\\\\\\[\\\\\\\\033[00m\\\\\\\\]\\\\\\\\$ \\\"/\" $HOME/.bashrc"
 	confirm_cmd "sed -i \"s/PS1='\\\${debian_chroot:+(\\\$debian_chroot)}.*h:.*/PS1=\\\"\\\\\\\\D{%Y%m%dT%H%M} \\\${debian_chroot:+(\\\$debian_chroot)}\\\\\\\\u@\\\\\\\\h:\\\\\\\\w\\\\\\\\\\\$(parse_git_branch)\\\\\\\\$ \\\"/\" $HOME/.bashrc"
 	touch "$script_dir/status/bash_set_up"
+fi
+
+
+# Set up kitty terminal emulator
+if [ -n "$(contains apt_installs kitty)" ] && [ ! -f "$script_dir/status/kitty_installed" ]; then
+	echo
+	if [ -f "$settings_dir/kitty.conf" ]; then
+		kitty_conf_path="$settings_dir/kitty.conf"
+	else
+		kitty_conf_path="$script_dir/kitty.conf"
+	fi
+	if [ ! -d "$HOME/.config/kitty" ]; then
+		confirm_cmd "mkdir $HOME/.config/kitty"
+	fi
+	confirm_cmd "cp -av $kitty_conf_path $HOME/.config/kitty"
 fi
 
 
